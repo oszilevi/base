fc85031ef0dc28306b562f772b7ff6a4c6eda000
oszilevi <oszilevi@gmail.com>
2021-03-04 22:14:25 +0100

    fixing error, new JUnit test #4

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -5,17 +5,15 @@ import org.junit.Before;
 import org.junit.Test;
 
 import hu.bme.mit.train.interfaces.TrainSensor;
-import hu.bme.mit.train.system.TrainSystem;
-
+import hu.bme.mit.train.sensor.TrainSensorImpl;
 
 public class TrainSensorTest {
     TrainSensor sensor;
 
 
     @Before
-    public void before() {
-        TrainSystem system = new TrainSystem();
-        sensor = system.getSensor();
+    public void before(){
+        sensor = new TrainSensorImpl();
     }
 
     @Test
